ceda281ff0a6d24f3d708591a37704e5f6cb2b85
jiaaaqi <e0425649@u.nus.edu>
2020-08-25 22:12:52 +0800

    implement Level 1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printHorizontalLine() {
+        System.out.println("    ——————————————————————————————————————————————————");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you? \n");
-        System.out.println("Bye. Hope to see you again soon! ");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        printHorizontalLine();
+
+        // echo loop
+        String command = in.nextLine();
+        while (!command.equals("bye")) {
+            printHorizontalLine();
+            System.out.println("    " + command);
+            printHorizontalLine();
+            command = in.nextLine();
+        }
+
+        printHorizontalLine();
+        System.out.println("    Bye. Hope to see you again soon!");
+        printHorizontalLine();
     }
 }
